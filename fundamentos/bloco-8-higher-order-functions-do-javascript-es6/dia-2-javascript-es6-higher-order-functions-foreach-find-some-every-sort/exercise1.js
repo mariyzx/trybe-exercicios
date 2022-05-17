5b08baa9aa64ae5d4576e07960c4ad8bdb3d90ba
Mariana <marinhomariana8@gmail.com>
2022-05-17 17:09:41 -0300

    Correção do exercício 1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
@@ -61,8 +61,22 @@ const books = [
     },
   ];
   
-  // Adicione o código do exercício aqui:
+  /* Adicione o código do exercício aqui:
+     Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA */
 
-  const authorBornIn1947 = books.find((book) => book.author.birthYear === 1947);
+  const expectedResult = [
+    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+    'Fundação - Ficção Científica - Isaac Asimov',
+    'Duna - Ficção Científica - Frank Herbert',
+    'A Coisa - Terror - Stephen King',
+    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  ];
+  
+  function formatedBookNames() {
+    // escreva seu código aqui
+    return books.map((book) => `${book.name} - ${book.genre} - ${book.author}`);
 
-  console.log(authorBornIn1947);
+  }
+
+  console.log(formatedBookNames());
