a3305943fdb5a64b673384d6518d2adef84bf3f8
Mariana <marinhomariana8@gmail.com>
2022-05-17 16:39:35 -0300

    Resolução dos exercícios de lógica de programção e algoritmos

---
+++ fundamentos/bloco-4-introduçao-javascript-logica-de-programaçao/dia-3-javascript-logica-de-programacao-e-algoritmos/1_programa/script.js
@@ -0,0 +1,15 @@
+let num = 10;
+
+let fat = 1
+
+for (let i = num; i > 1; i-=1) {
+     fat *= i // fat = fat * i
+     console.log(fat)
+    
+}
+
+console.log(fat)
+
+// o laço for inicia em 5 e vai decrescendo até 2
+// o valor de fat inicia em 1 e a cada descrescimento
+// ele pega o valor anterior e multiplica por um decrescimento
---
+++ fundamentos/bloco-4-introduçao-javascript-logica-de-programaçao/dia-3-javascript-logica-de-programacao-e-algoritmos/3_programa/script.js
@@ -0,0 +1,7 @@
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// maior palavra
+
+let maiorPalavra = array[0];
+
+
