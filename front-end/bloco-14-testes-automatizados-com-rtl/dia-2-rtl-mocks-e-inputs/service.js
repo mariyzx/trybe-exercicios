829ad30f2403649dddb98f135b882cdb6c93ffae
Mariana Werneck <marinhomariana8@gmail.com>
2022-07-14 19:57:14 -0300

    Exercício 1 - parte 4

--- front-end/bloco-14-testes-automatizados-com-rtl/dia-2-rtl-mocks-e-inputs/service.js
+++ front-end/bloco-14-testes-automatizados-com-rtl/dia-2-rtl-mocks-e-inputs/service.js
@@ -2,4 +2,16 @@ randomNumber = () => {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-module.exports = { randomNumber };
+returnCaps = (string) => {
+  return string.toUpperCase()
+}
+
+returnFirstLetter = (string) => {
+  return string.charAt(0)
+}
+
+returnConcat = (str1, str2) => {
+  return str1 + ' ' + str2;
+}
+
+module.exports = { randomNumber, returnCaps, returnFirstLetter, returnConcat };
--- front-end/bloco-14-testes-automatizados-com-rtl/dia-2-rtl-mocks-e-inputs/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-rtl/dia-2-rtl-mocks-e-inputs/service.test.js
@@ -1,5 +1,4 @@
 var service = require('./service');
-jest.mock("./service");
 
 it('Testa se a função randomNumber está sendo chamada, seu retorno e qnts vezes foi chamada', () => {
   service.randomNumber = jest
@@ -18,3 +17,45 @@ it('Testa se a função foi chamada e se a nova implementação foi aplicada ape
   expect(service.randomNumber).toHaveBeenCalled();
   expect(service.randomNumber).toHaveBeenCalledTimes(1);
 });
+
+it('Mockando função para receber 3 parâmetros e retornar multiplicação', () => {
+  service.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+  expect(service.randomNumber(1, 2, 3)).toBe(6);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+});
+
+it('Mockando função para receber 1 parâmetros e retornar seu dobro', () => {
+  service.randomNumber.mockReset();
+
+  service.randomNumber = jest.fn().mockImplementation((a) => a * 2);
+
+  expect(service.randomNumber(2)).toBe(4);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+});
+
+it('Mockando a função returnCaps para caixa baixa', () => {
+  const returnCaps = jest.spyOn(service, 'returnCaps').mockImplementation((str) => str.toLowerCase());
+
+  expect(returnCaps('AAA')).toBe('aaa');
+});
+
+it('Mockando a função returnFirstLetter para retornar a última letra', () => {
+  const returnFirstLetter = jest.spyOn(service, 'returnFirstLetter').mockImplementation((str) => str.charAt(str.length - 1));
+
+  expect(returnFirstLetter('teste')).toBe('e');
+});
+
+it('Mockando a função returnConcat para concatenar três strings', () => {
+  const returnConcat = jest.spyOn(service, 'returnConcat').mockImplementation((str1, str2, str3) => str1 + str2 + str3);
+
+  expect(returnConcat('t', 'r', 'y')).toBe('try');
+});
+
+test("restaurando a primeira função e verifica se ela retorna em caixa alta", () => {
+  service.returnCaps.mockRestore();
+
+  expect(service.returnCaps("lowercase")).toBe("LOWERCASE");
+})
