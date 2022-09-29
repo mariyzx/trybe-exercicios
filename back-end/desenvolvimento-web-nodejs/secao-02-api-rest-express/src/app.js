7c6159dd5a609cf1988f25075d2fcb27eba6a854
Mariana Werneck <marinhomariana8@gmail.com>
2022-09-29 10:32:31 -0300

    Criando um endpoint do tipo GET com que lista todas as atividades do array por id

--- back-end/desenvolvimento-web-nodejs/secao-02-api-rest-express/src/app.js
+++ back-end/desenvolvimento-web-nodejs/secao-02-api-rest-express/src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const activities = require('./data/activities');
 
 const app = express();
 
 app.use(express.json());
 
 // ROTAS
+
+app.get('/myActivities/:id', (req, res) => {
+  const idParam = req.params.id;
+  const activity = activities.find(({ id }) => id === Number(idParam));
+  res.status(200).json(activity);
+});
 
 module.exports = app;
