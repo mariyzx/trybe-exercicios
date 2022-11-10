6dd70cfe157ca4bcc729f771ec94f944b5456c51
Mariana Werneck <marinhomariana8@gmail.com>
2022-11-10 13:02:36 -0300

    Adicionando métodos á classe de Data

--- back-end/secao-poo-solid/dia-1-introduçao-a-orientaçao-a-objetos/src/Date.ts
+++ back-end/secao-poo-solid/dia-1-introduçao-a-orientaçao-a-objetos/src/Date.ts
@@ -1,5 +1,43 @@
-export default class Date {
-  public dia = number;
-  public mes = number;
-  public ano = number;
+export default class Data {
+  public _dia: number;
+  public _mes: number;
+  public _ano: number;
+
+  constructor(dia: number, mes: number, ano: number) {
+    const dataStr = `${dia}-${mes}-${ano}`;
+
+    if (new Date(dataStr).getDate() != dia) {
+      this._dia = 1;
+      this._mes = 1;
+      this._ano = 1900
+    } else {
+      this._dia = dia;
+      this._mes = mes;
+      this._ano = ano;
+    }
+  }
+
+  get dia(): number {
+    return this._dia;
+  }
+
+  set dia(value: number) {
+    this._dia = value;
+  }
+
+  get mes(): number {
+    return this._mes;
+  }
+
+  set mes(value: number) {
+    this._mes = value;
+  }
+
+  get ano(): number {
+    return this._ano;
+  }
+
+  set ano(value: number) {
+    this._ano = value;
+  }
 }
